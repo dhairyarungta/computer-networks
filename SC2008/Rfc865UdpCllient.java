19574f1fc5e8be382ae43208cc373da031bec871
dhairyarungta <97079960+dhairyarungta@users.noreply.github.com>
2023-02-25 22:07:45 +0800

    lab2

---
+++ SC2008/Rfc865UdpCllient.java
@@ -0,0 +1,49 @@
+import java.io.IOException;
+import java.net.*;
+
+public class Rfc865UdpCllient
+{
+    /*
+    Name: Harvey Specter
+    Group: Always1
+    IP Address: 155.23.14.254
+     */
+    public static void main(String[] args) throws SocketException, UnknownHostException
+    {
+
+        //
+        // 1. Open UDP socket
+        //
+
+        DatagramSocket socket = new DatagramSocket();
+        // InetAddress address = InetAddress.getByName("hwlab1.scse.ntu.edu.sg");
+        InetAddress address = InetAddress.getByName("localhost");
+
+        try
+        {
+
+         //
+         // 2. Send UDP request to the server
+         //
+
+          String msg = new String("Dhairya rungta, A21");
+          byte[] buffer = msg.getBytes();
+
+            DatagramPacket request = new DatagramPacket(buffer, buffer.length, address, 17);
+            socket.send(request);
+
+
+
+         //
+         // 3. Receive UDP reply from the server
+         //
+
+
+            DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
+            socket.receive(reply);
+            String received = new String(reply.getData(), 0, reply.getLength());
+            System.out.println(received);
+
+        } catch(IOException e) {}
+    }
+}
---
+++ SC2008/Rfc865UdpServer.java
@@ -0,0 +1,54 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
+public class Rfc865UdpServer{
+    static int QOTD  = 17 ;
+    public static void main (String[]args){
+        DatagramSocket socket= null;
+        try {
+            socket = new DatagramSocket(QOTD);
+            System.out.println("UDP server listening to "+ QOTD);
+
+        }catch(SocketException e){
+            e.printStackTrace();
+        }
+
+        try {
+            while(true){
+                //Listen to UDP request from client 
+                byte[] buffer = new byte [512];
+                byte[] buffer2 = new byte [512];
+                DatagramPacket request = new DatagramPacket(buffer,buffer.length);
+                System.out.println("waiting for reuqest");
+
+                socket.receive(request);
+                String received = new String(request.getData(), 0, request.getLength());
+                System.out.println(received);
+
+                InetAddress clientAddress = request.getAddress();
+                int clientPort = request.getPort();
+
+                //Send request back to client 
+
+                String replyString = "i love hooli";
+                byte [] replyBuf = replyString.getBytes("UTF-8");
+
+                DatagramPacket reply = new DatagramPacket(replyBuf,replyBuf.length,clientAddress,clientPort);
+                System.out.println("Sending reply to client");
+                socket.send(reply);
+                System.out.println("reply sent by socket");
+
+            }
+        }
+        catch (IOException e){
+            e.printStackTrace();
+
+        }
+        finally{
+            socket.close();
+        }
+    }
+}
